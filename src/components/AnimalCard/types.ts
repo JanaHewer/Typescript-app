6935229c6c36305d617c000e50c1243810038eb5
JanaHewer <152711200+JanaHewer@users.noreply.github.com>
2024-04-24 11:12:21 +0200

    Homework06

---
+++ src/components/AnimalCard/types.ts
@@ -0,0 +1,18 @@
+// type Animal{
+//   name: string,
+//   species: string,
+//   image: string
+// }
+
+
+interface Animal{
+    name: string,
+    species: string,
+    image: string
+  }
+  
+  
+ export interface AnimalCardProps {
+    animalData: Animal
+  }
+  
