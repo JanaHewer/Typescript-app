bd998774340e8b5d01925ca68f407e6fb2f7b11e
JanaHewer <152711200+JanaHewer@users.noreply.github.com>
2024-05-07 12:53:32 +0200

    Lesson09

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import "./App.css";
+
+import GlobalStyles from "styles/GlobalStyles";
 
 // Lessons:
 // import Lesson06 from "./lessons/Lessons06/Lesson06";
 // import Lesson07 from "./lessons/Lesson07/Lesson07";
 // import Lesson08 from "lessons/Lesson08/Lesson08";
+import Lesson09 from "lessons/Lesson09/Lesson09";
 // Homeworks:
-import Homework08 from "homeworks/Homework08/Hmework08";
+// import Homework08 from "homeworks/Homework08/Hmework08";
 // Consultations:
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles/>
       {/* <Lesson06 /> */}
       {/* <Lesson07/> */}
       {/* <Lesson08/> */}
-      <Homework08 />
-    </div>
+      {/* <Homework08 /> */}
+      <Lesson09/>
+    </>
   );
 }
 
